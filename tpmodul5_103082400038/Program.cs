bd5a3ca764bc4ab0a911bb7c289b46ba7b0df5d6
MadisBinIsmad <dimasdewandaru31@gmail.com>
2026-03-27 12:52:08 +0700

    merge branch generic-method dan generic-class

--- tpmodul5_103082400038/Program.cs
+++ tpmodul5_103082400038/Program.cs
@@ -6,6 +6,11 @@ class Program
     {
         HaloGeneric halo = new HaloGeneric();
         halo.SapaUser<string>("Dimas");
+
+        Console.WriteLine("");
+
+        DataGeneric<string> data = new DataGeneric<string>("103082400038");
+        data.PrintData();
     }
 }
 
@@ -14,5 +19,20 @@ class HaloGeneric
     public void SapaUser<T>(T user)
     {
         Console.WriteLine("Halo user " + user);
+    }
+}
+
+class DataGeneric<T>
+{
+    private T data;
+
+    public DataGeneric(T data)
+    {
+        this.data = data;
+    }
+
+    public void PrintData()
+    {
+        Console.WriteLine("Data yang tersimpan adalah: " + data);
     }
 }
--- tpmodul5_103082400038/obj/Debug/net10.0/tpmodul5_103082400038.AssemblyInfo.cs
+++ tpmodul5_103082400038/obj/Debug/net10.0/tpmodul5_103082400038.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tpmodul5_103082400038")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d9a9290bae389a3bd275142c8220c433f28d7d6")]
 [assembly: System.Reflection.AssemblyProductAttribute("tpmodul5_103082400038")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tpmodul5_103082400038")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
